f6faeb495e52e62b7612c99d7f43a03a3b656b7d
minhtuan2001 <minhtuandev2001@gmail.com>
2024-05-17 23:14:14 +0700

    config socket in project

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 require("dotenv").config();
-const cors = require("cors")
+const cors = require("cors");
 const connectDB = require("./src/config/db");
 const router = require("./src/routes/client/index.route");
-const corsOptions = require("./src/config/cors");
+const socketConfig = require("./src/socket/socket");
 
 connectDB()
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+const http = require("http");
+const server = http.createServer(app)
+socketConfig(server)
 
 app.use(cors());
 app.use(express.json());
@@ -19,6 +22,6 @@ app.post("/", (req, res) => {
   res.send("chao")
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("Listening on port", PORT);
 })
